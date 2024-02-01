6a5515607afa31ec13689076058e240b7f2c13b2
Yo-nathan <158175084+Yo-nathan@users.noreply.github.com>
2024-02-01 08:51:45 -0500

    Name print exercises

---
+++ NamePrintExecrise.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<"First_Name Last_Name"<<endl;
+
+    cout<<"First_Name"<<endl;
+    cout<<"Last_Name"<<endl;
+
+    cout<<"First_Name\nLast_Name"<<endl;
+
+    cout<<"F\ni\nr\ns\nt\n_\nN\na\nm\ne\n";
+
+    cout<<"First_Name\tLast_Name"<<endl;
+
+    cout<<"First_Name\\Last_Name"<<endl;
+
+    return 0;
+}
